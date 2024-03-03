3bd000deaa7d79615077f9cd51836820a30c6c23
Reissd <reissd@csp.edu>
2024-03-03 08:05:04 -0600

    database connection config signup.php

--- Capstone/signup.php
+++ Capstone/signup.php
@@ -8,7 +8,7 @@
         <?php
         define('DB_SERVER', 'localhost');
         define('DB_USERNAME', 'root');
-        define('DB_PASSWORD', '');
+        define('DB_PASSWORD', 'mysql');
         define('DB_NAME', 'capstonefish');
         $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
