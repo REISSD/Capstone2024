24e9f8276a754de82e0b02a6d4d8302815114801
Reissd <reissd@csp.edu>
2024-03-03 08:00:34 -0600

    changed database name signup.php

--- Capstone/signup.php
+++ Capstone/signup.php
@@ -8,8 +8,8 @@
         <?php
         define('DB_SERVER', 'localhost');
         define('DB_USERNAME', 'root');
-        define('DB_PASSWORD', 'mysql');
-        define('DB_NAME', 'capstone');
+        define('DB_PASSWORD', '');
+        define('DB_NAME', 'capstonefish');
         $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
         if ($conn->connect_error) {
