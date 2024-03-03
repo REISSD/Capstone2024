4a648f0ddd2e67a1989851b51f9b042d2b6a73c8
Wang Lee <leew2@csp.edu>
2024-03-02 22:39:56 -0600

    Updated database Added an "comment" tables for the user to review and leave messages for the products For such reason, one can gete the review through the product that it's on or the user this will allow us to see all the review for the products and see all review done by the member

--- Capstone/products.php
+++ Capstone/products.php
@@ -9,7 +9,7 @@
     //db
         define('DB_SERVER', 'localhost');
         define('DB_USERNAME', 'root');
-        define('DB_PASSWORD', '');
+        define('DB_PASSWORD', 'mysql');
         define('DB_NAME', 'capstone');
         $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
